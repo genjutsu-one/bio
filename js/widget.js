db7e116ebbaba273938d64c6d3d89973caf89047
vpnsakura <dmitriisupersell@gmail.com>
2026-06-20 19:24:00 +0300

    Update widget.js

--- js/widget.js
+++ js/widget.js
@@ -1,8 +1,23 @@
+
+const widgetBase = 'https://raw.githubusercontent.com/genjutsu-one/bio/main/media/widget';
+const widgetCount = 9;
+
+function pickWidgetUrl() {
+  const n = Math.floor(Math.random() * widgetCount) + 1;
+  return n === 1 ? widgetBase + '.mp4' : widgetBase + n + '.mp4';
+}
+
 export function initWidget() {
   const vid = document.getElementById('widget-vid');
   if (!vid) return;
 
   vid.play().catch(() => {});
+
+  setInterval(() => {
+    vid.src = pickWidgetUrl();
+    vid.load();
+    vid.play().catch(() => {});
+  }, 30 * 60 * 1000);
 
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
